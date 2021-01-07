1e54ccc25783eb0feea31e5d52d345dbfa652dd3
chelseashin <chaewonshin95@gmail.com>
2021-01-07 18:52:23 +0900

    210107 | Algorithm Problem Solving

--- chelseashin/SamsungProblems/20058_마법사상어와파이어스톰.py
+++ chelseashin/SamsungProblems/20058_마법사상어와파이어스톰.py
@@ -5,6 +5,7 @@
 # 뒷 부분은 금방 풀 줄 알았지만 촘촘하게 짜지 않고 대강 구상만 하고 시작하여 잔 실수가 많았음
 # 특히 얼음 녹이는 부분을 제대로 이해하지 못해 계속 답이 틀림.
 # 문제 꼼꼼하게 읽고 풀기 전에 고려할 부분들 미리 적어놓기
+# 각 동작 함수화한 것이 시간, 메모리 단축에 도움
 
 import sys
 sys.stdin = open("20058_input.txt")
@@ -15,30 +16,24 @@ from collections import deque
 dr = (-1, 1, 0, 0)
 dc = (0, 0, -1, 1)
 
-N, Q = map(int, input().split())
-A = [list(map(int, input().split())) for _ in range(2**N)]
-L = list(map(int, input().split()))
-
-# 파이어스톰 총 Q번 시전
-for size in L:
-    # (0, 0)에서 간격(2**size)만큼이 배열회전의 시작위치(r, c)가 됨
+def firestorm(A):
+    # (0, 0)에서 간격(2**size)만큼이 배열 회전의 시작위치(r, c)가 됨
     new = [[0] * 2**N for _ in range(2**N)]
-    gap = 2**size
+    gap = 2 ** size
     for r in range(0, 2**N, gap):
         for c in range(0, 2**N, gap):
             for i in range(gap):
-                temp = A[r+i][c:c + gap]
+                temp = A[r+i][c:c+gap]
                 for j in range(gap):
-                    new[r+j][c+gap-i-1] = temp[j]
+                    new[r+j][c + gap-i-1] = temp[j]
+    return new
 
-    A = [x[:] for x in new]     # new에 만든 회전 후 모습 A에 반영
-
-    # 시전 후
-    melt = []   # 얼음 녹는 좌표 저장 리스트
+def melting_ice():
+    melt = []  # 얼음 녹는 좌표 저장 리스트
     for sr in range(2**N):
         for sc in range(2**N):
             if A[sr][sc]:
-                cnt = 0     # 주변이 0인 것의 갯수
+                cnt = 0  # 주변이 0인 것의 갯수
                 for d in range(4):  # 4방향 탐색
                     nr = sr + dr[d]
                     nc = sc + dc[d]
@@ -48,18 +43,30 @@ for size in L:
                     # 주변이 0인 것을 세다 보니 모서리가 안 세졌음..
                     if A[nr][nc]:
                         cnt += 1
-                    if cnt >= 3:    # 이 부분을 이상하게 해석..
+                    if cnt >= 3:  # 이 부분을 이상하게 해석..
                         break
-                if cnt < 3:    # 3면 이상이 얼음이 아니라면 melt에 담기
+                if cnt < 3:  # 3면 이상이 얼음이 아니라면 melt에 담기
                     melt.append((sr, sc))
     # 얼음 녹이기
-    for (r, c) in melt:
-        A[r][c] -= 1
+    for (mr, mc) in melt:
+        A[mr][mc] -= 1
+
+# main
+N, Q = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(2**N)]
+L = list(map(int, input().split()))
+
+# 파이어스톰 총 Q번 시전
+for size in L:
+    # 시전
+    A = firestorm(A)
+    # 시전 후 얼음 녹이기
+    melting_ice()
 
 # 얼음의 칸 세는 함수
 def bfs(sr, sc):
     island = 1
-    visited[sr][sc] = 1     # 말도 안 되는 실수.. visited[sr][sr]로 써놓고 오랫동안 못 찾음, 이런 실수 하지 말자
+    visited[sr][sc] = 1     # 실수 금지
     Q = deque([(sr, sc)])
     while Q:
         r, c = Q.popleft()
@@ -82,7 +89,7 @@ for i in range(2**N):
     for j in range(2**N):
         iceCnt += A[i][j]
         if A[i][j] and not visited[i][j]:
-        #     # bfs 함수 - 얼음 덩어리 칸의 갯수 리턴
+            # 얼음 덩어리 칸의 갯수 리턴
             maxCnt = max(maxCnt, bfs(i, j))
 print(iceCnt)
 print(maxCnt)
