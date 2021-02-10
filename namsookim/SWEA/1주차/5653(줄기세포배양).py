54a784689505d840715527c1c3bbbcf3f6b683c8
김남수 <aowlrehrk3@naver.com>
2021-02-10 13:09:51 +0900

    21

--- namsookim/SWEA/1주차/5653(줄기세포배양).py
+++ namsookim/SWEA/1주차/5653(줄기세포배양).py
@@ -1,7 +1,5 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXRJ8EKe48DFAUo
 
-
-
 import sys
 import copy
 from collections import deque
@@ -12,22 +10,16 @@ def print_array(array):
             print(array[i][j],end=' ')
         print()
 
-N, M,K = map(int,input().split())
+N,M,K = map(int,input().split())
 max_array = [[0]*(M+(2*K)) for _ in range(N+(2*K))]
-#print(max_array)
+
 array = [list(map(int,input().split())) for _ in range(N)]
-
-# 배열 늘리기
-
-# 언제 부터 넣냐?
 
 for i in range(K,K+N):
     for j in range(K,K+M):
         max_array[i][j] = array[K-i][K-j]
-#print_array(max_array)
 
 def active_check(non_active_cell,active_cell):
-    #print(non_active_cell)
     temp = []
     for i in range(len(non_active_cell)):
         x,y,index,time=non_active_cell[i]
@@ -69,11 +61,10 @@ def bfs(array,active_cell,non_active_cell):
                     non_active_cell.append((nx,ny,index,index))
 
                 elif new_array[nx][ny]>0: # 누가 있으면 크기비교
-                    print('겹치네?')
+
                     # 생명력 높은 세포만 그 자리 차지
                     if new_array[nx][ny] < index:
-                        print(non_active_cell,' 여기에서')
-                        print(nx,ny,array[nx][ny],index,' 이거 제거')
+
                         non_active_cell.remove((nx, ny, array[nx][ny],index)) # 현재 있는 놈 바뀌니 비활성 세포에서 제거
 
                         new_array[nx][ny] = index
@@ -95,7 +86,6 @@ def solve(array):
 
     for t in range(K+1): # K시간 후
         # 현재 array -> 시간 반영
-        
 
         new_array,remain_cell = bfs(array, active_cell, non_active_cell) # 시간이 0인 놈 활성화 되고 번식
         array = new_array
--- namsookim/Samsung/4주차/17142(연구소 3)_2.py
+++ namsookim/Samsung/4주차/17142(연구소 3)_2.py
@@ -1,6 +1,5 @@
 import sys
 from itertools import combinations
-import copy
 from collections import  deque
 # https://www.acmicpc.net/problem/17142
 
@@ -21,112 +20,88 @@ from collections import  deque
 
 풀이
 1. 내장함수 combinations 사용해서 경우의 수를 구함
-2. 활성화 바이러스에서 bfs를 통해 최소 거리 찾음.
-3. bfs를 통해 구한 최대 시간을 반환한 뒤, 현재까지의 최솟값과 비교
-4. 모든 경우의 수를 따졌는데 빈칸에 바이러스 못 퍼뜨리면 -1 반환
+2. 활성화 바이러스에서 bfs를 통해 최소 거리 찾음.(바뀜)
+3. bfs를 통해 구한 최대 시간을 반환한 뒤, 현재까지의 최솟값과 비교(바뀜)
+4. 모든 경우의 수를 따졌는데 빈칸에 바이러스 못 퍼뜨리면 -1 반환 (바뀜)
 
 시행착오
 1. 비활성화된 바이러스를 통과하지 못한다고 구현했음.
+
+스터디 후기
+
+바뀐 부분
+1. 맵, bfs 부분 바로 처리. (따로 하지 않고 한번에) (시간효율 up)
+2. 빈칸 개수를 초기에 찾아내서 변수에 저장. total_blank
+3. check= [[-1]*N for _ in range(N)] 으로 해서 활성부분만 0으로 바꿈. 이것으로 거리 구함
+4. cnt가 total_blank가 되었을 때 바로 값 return 이때가 최고 시간 값임.
+
 """
 
 # 0은 빈칸, 1은 벽, 2는 바이러스
+
 input = sys.stdin.readline
 INF = int(1e9)
 N, M = map(int,input().split())
 virus = []
-
+total_blank = 0
 array = [list(map(int,input().split())) for _ in range(N)]
 for i in range(len(array)):
     for j in range(len(array[0])):
+
         if array[i][j] == 2:
-            virus.append((i,j,0)) # 0은 이동거리값(bfs에서 활용)
-
-def active_function(active_virus,array):
-    new_array = copy.deepcopy(array)
-    for i in range(N):
-        for j in range(N):
-            if new_array[i][j] == 0:
-                new_array[i][j] = -1 # 갈수 있는 곳 -1
-
-            elif new_array[i][j] == 1: # 벽인 경우
-                new_array[i][j] = '-' # -로 표시
-
-            elif new_array[i][j] == 2:
-                new_array[i][j] = '*' # 바이러스 처리
-
-    for x,y,num in active_virus:
-        new_array[x][y] = 0 # 활성화 바이러스 처리
-
-    return new_array
+            virus.append((i,j)) # 0은 이동거리값(bfs에서 활용)
+        elif array[i][j] == 0:
+            total_blank += 1
 
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
 
 def bfs(virus,array):
+    global total_blank
 
-    max_num = 0
-    check = [[False]*N for _ in range(N)]
+    cnt = 0
+
+    check = [[-1]*N for _ in range(N)]
+    for vx, vy in virus:
+        check[vx][vy] = 0
 
     q=deque(list(virus))
 
     while q:
-        vx,vy,time=q.popleft()
-
-        check[vx][vy] = True
+        vx,vy=q.popleft()
 
         for i in range(4):
             nx = vx+dx[i]
             ny = vy+dy[i]
             if 0<=nx<N and 0<=ny<N: # 범위 안에 들고
-                if check[nx][ny] != True and (array[nx][ny] == -1 or array[nx][ny] == '*'): # 바이러스도 건너뛸 수 있음. 이거 처리 못했음.
+                if check[nx][ny] == -1 and (array[nx][ny] == 0 or array[nx][ny] == 2): # 바이러스도 건너뛸 수 있음. 이거 처리 못했음.
 
-                    if array[nx][ny] == -1:
-                        array[nx][ny] = time+1
+                    if array[nx][ny] == 0:
+                        cnt += 1
 
-                    check[nx][ny] = True
-                    q.append((nx,ny,time+1))
+                    check[nx][ny] = check[vx][vy] +1
+                    if cnt == total_blank:
+                        return check[nx][ny]
 
-    for i in range(N):
-        for j in range(N):
-            if array[i][j] == '*' or array[i][j] == '-':
-                continue
-            if max_num<array[i][j]:
-                max_num = array[i][j]
+                    q.append((nx,ny))
 
-    return max_num
-
-
-def print_array(array):
-    for i in range(len(array)):
-        for j in range(len(array[0])):
-            print(array[i][j],end=' ')
-        print()
-
-def possible(array):
-    for i in range(len(array)):
-        for j in range(len(array[0])):
-            if array[i][j] == -1:
-                return False
-    return True
+    return INF
 
 def solve():
+    global total_blank
+
     answer = INF
     virus_combi=list(combinations(virus,M)) # 전체 비활성 바이러스 중 M개를 선택하는 경우의 수
-    tag = False # 가능한게 없는지 체크
+    if total_blank == 0:
+        return 0
 
     for active_virus in virus_combi:
 
-        time_array=active_function(active_virus,array) # 선택한 M개를 활성화
-        min_time = bfs(active_virus, time_array)
+        min_time = bfs(active_virus, array)
+        answer = min(min_time,answer)
 
-        if possible(time_array): # 빈 칸없이 채워졌으면
-            tag = True
-            # 최대값 반환한 뒤 비교
-            answer = min(min_time,answer)
-
-    if tag ==False: # 가능한게 없으면
+    if answer == INF:
         return -1
-
     else:
         return answer
 
