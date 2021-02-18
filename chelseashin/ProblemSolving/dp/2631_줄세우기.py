24e0925a65f5138790a86b4766ca4bd4a7ac0d99
chelseashin <chaewonshin95@gmail.com>
2021-02-18 19:53:23 +0900

    210218 | Algorithm Problem Solving

--- chelseashin/ProblemSolving/dp/2631_줄세우기.py
+++ chelseashin/ProblemSolving/dp/2631_줄세우기.py
@@ -11,34 +11,33 @@ A = [int(input()) for _ in range(N)]
 # print(A)
 
 # 풀이 1
-# dp = [0] * N
-# dp[0] = 1
-# for i in range(1, N):
-#     temp = []
-#     for j in range(i):
-#         if A[i] > A[j]:
-#             temp.append(dp[j])
-#     if not temp:
-#         dp[i] = 1
-#     else:
-#         dp[i] = max(temp) + 1
-# # print(dp)
-# print(N - max(dp))
+dp = [0] * N
+dp[0] = 1
+for i in range(1, N):
+    temp = []
+    for j in range(i):
+        if A[i] > A[j]:
+            temp.append(dp[j])
+    if not temp:
+        dp[i] = 1
+    else:
+        dp[i] = max(temp) + 1
+print(dp)
+print(N - max(dp))
 
 
 # 풀이 2
-lis = [A[0]]
-temp = 0
-for n in range(1, N):
-    for idx, val in enumerate(lis):
-        if A[n] > val and A[n] not in lis:
-            temp = A[n]
-        elif A[n] < val and A[n] not in lis:
-            lis[idx] = A[n]
-            temp = 0
-
-    if temp:
-        lis.append(temp)
-    temp = 0
-
-print(N-len(lis))
+# lis = [A[0]]
+# temp = 0
+# for n in range(1, N):
+#     for idx, val in enumerate(lis):
+#         if A[n] > val and A[n] not in lis:
+#             temp = A[n]
+#         elif A[n] < val and A[n] not in lis:
+#             lis[idx] = A[n]
+#             temp = 0
+#
+#     if temp:
+#         lis.append(temp)
+#     temp = 0
+# print(N-len(lis))
