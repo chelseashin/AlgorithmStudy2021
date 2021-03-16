f33db7dd96156b224ff25501202af6a473060701
chelseashin <chaewonshin95@gmail.com>
2021-03-17 00:05:27 +0900

    210317 | LINE Problem Solving

--- chelseashin/ProblemSolving/LINE/05.py
+++ chelseashin/ProblemSolving/LINE/05.py
@@ -1,13 +1,116 @@
-# 23:50 start
+# 유저 생성
+class User():
+    def __init__(self):
+        self.cards = []
+        self.sum = 0
+        # 11은 최대 하나니까 적용할 변수 하나 둬서 처리하기
+        self.maxSum = 0
+        
+    def add(self, card):
+        if card > 10:   # J, Q, K 처리 
+            card = 10
+        if card == 1:   # A 처리, 11로 계산했을 때의 값을 따로 저장
+            self.maxSum += 10
+        self.cards.append(card)
+        self.sum += card
+        self.maxSum += card
+        
+def battle(player, dealer):
+    # 11 사용해서 21 이하이면 A를 11로 사용한 것으로 바꿔줌
+    if player.maxSum <= 21:
+        player.sum = player.maxSum
+    if dealer.maxSum <= 21:
+        dealer.sum = dealer.maxSum
+    # 비김
+    if player.sum == dealer.sum:
+        return 0
+    # 패
+    elif player.sum < dealer.sum:
+        return -2
+    # 승
+    else:
+        if player.sum == 21:    # 블랙잭
+            return 3
+        else:
+            return 2
+
+def giveCard(user, cards):
+    global idx
+    user.add(cards[idx])
+    idx += 1
+
+# 1로 사용할지, 11로 사용할지 정해서 계산
+# limit 이상 될 때까지 카드 받기
+def canGive(user, limit):
+    # 21 넘어가면 11로 사용 안 함
+    # 11 사용하면 maxSum으로 보고, 1 사용하면 sum으로 봄
+    if user.maxSum > 21:
+        if user.sum < limit:    # limit 넘지 않으면 카드 줄 수 있음
+            return True
+        else:
+            return False
+    else:   # 11로 사용
+        if user.maxSum < limit:
+            return True
+        else:
+            return False
+
+def play(cards):
+    global idx, score
+    player = User()
+    dealer = User()
+    # 처음 한 장씩 두번 받음
+    for i in range(4):
+        if i % 2 == 0:      # 플레이어가 받음
+            giveCard(player, cards)
+        else:
+            giveCard(dealer, cards)
+    # 딜러의 두 번째 카드 오픈
+    openCard = dealer.cards[1]
+
+    # player 카드 더 받을지 확인
+    while True:
+        if player.sum == 21 or player.maxSum == 21:   # 둘다 카드 합 21
+            return battle(player, dealer)   # 누가 이겼는지 점수 리턴
+        elif player.sum > 21:
+            return -2           # 21 넘어가면 즉시 패배
+        else:
+            if (openCard == 1 or openCard >= 7):
+                if canGive(player, 17):     # 카드 합 17이상 될 때까지 받음
+                    giveCard(player, cards)
+                else:
+                    break
+            elif openCard in [4, 5, 6]:     # 플레이어 카드 받지 X
+                break
+            elif openCard in [2, 3]:
+                if canGive(player, 12):     # 카드 합 12이상 될 때까지 받음
+                    giveCard(player, cards)
+                else:
+                    break
+    # dealer 카드 더 받을지 확인
+    while True:
+        if dealer.sum > 21:         # 딜러 카드 합 21 넘으면 즉시 패배
+            return 2
+        if canGive(dealer, 17):     # 딜러 카드 합 17 넘을 때까지 카드 받기
+            giveCard(dealer, cards)
+        else:                       # 17 넘으면 받기 중단
+            break
+    # 이번 게임 종료
+    return battle(player, dealer)
 
 def solution(cards):
-    
-
-
+    global idx, score
+    idx, score = 0, 0
+    while True:
+        # 예외처리로 카드 인덱스 넘기면서 끝나도록
+        try:        # play()에서 예외 발생하지 않으면 점수 리턴,
+            score += play(cards)
+        except:     # 발생하면 점수 출력하고 종료
+            return score
 print(solution([12, 7, 11, 6, 2, 12]))
-# print(solution([1, 4, 10, 6, 9, 1, 8, 13]))
-# print(solution([10, 13, 10, 1, 2, 3, 4, 5, 6, 2]))
-# print(solution([3, 3, 3, 3, 3, 3, 3, 3, 3, 3]))
+print(solution([1, 4, 10, 6, 9, 1, 8, 13]))
+print(solution([10, 13, 10, 1, 2, 3, 4, 5, 6, 2]))
+print(solution([3, 3, 3, 3, 3, 3, 3, 3, 3, 3]))
 
 # 2
 # 1
--- chelseashin/ProblemSolving/LINE/06.py
+++ chelseashin/ProblemSolving/LINE/06.py
@@ -1,6 +1,54 @@
 def solution(companies, applicants):
     result = []
+    # print(companies)
+    # print(applicants)
+    compChoices = dict()    # 기업별 채용 인원 수
+    compInfo = dict()       # 기업별 지원자 선호 순위
+    for info in companies:
+        compChoices[info[0]] = int(info[-1])
+        for i, v in enumerate(info[2:-2]):
+            if info[0] not in compInfo.keys():
+                compInfo[info[0]] = [v]
+                # compInfo[info[0]] = [(i, v)]
+            else:
+                compInfo[info[0]].append(v)
+                # compInfo[info[0]].append((i, v))
+    print("기업 정보")
+    print(compChoices)
+    print(compInfo)
+    appChoices = dict()
+    appInfo = dict()
+    for info in applicants:
+        appChoices[info[0]] = int(info[-1])
+        for i, v in enumerate(info[2:-2]):
+            if info[0] not in appInfo.keys():
+                appInfo[info[0]] = [(i, v)]
+            else:
+                if i < appChoices[info[0]]:
+                    appInfo[info[0]].append((i, v))
+    print("지원자 정보")
+    print(appChoices)
+    print(appInfo)
+    curMatching = {comp: [] for comp in compChoices.keys()}
 
+    for idx, values in appInfo.items():
+        if appChoices[idx]:
+            for pt, comp in values:
+                curMatching[comp].append((compInfo[comp].index(idx), idx))
+                appChoices[idx] -= 1
+                print(pt, comp)
+                break
+    rejected = []
+    for idx, values in curMatching.items():
+        curMatching[idx].sort()
+        while len(values) > compChoices[idx]:
+            rejected.append(curMatching[idx].pop()[1])
+    print("현재 매칭 상태", curMatching, "거절", rejected)
+    print(appChoices)
+
+    while rejected:
+
+    #     break
     return result
 
 print(solution(["A fabdec 2", "B cebdfa 2", "C ecfadb 2"], ["a BAC 1", "b BAC 3", "c BCA 2", "d ABC 3", "e BCA 3", "f ABC 2"]))
