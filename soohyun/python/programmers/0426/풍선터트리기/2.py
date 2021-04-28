6acb220ef7e94319abb22c52b28d9682af17dfea
kimSooHyun950921 <soohyunkim950921@gmail.com>
2021-04-28 12:52:04 +0900

    add 풍선터뜨리기

---
+++ soohyun/python/programmers/0426/풍선터트리기/2.py
@@ -0,0 +1,10 @@
+def solution(a):
+    if len(a) >= 3:
+        answer = 2
+        last = len(a)
+        for i in range(1, last-1):
+            if a[i] <= min(a[0:i]) or a[i] <= min(a[i+1:last+1]):
+                answer += 1
+        return answer
+    else:
+        return len(a)
