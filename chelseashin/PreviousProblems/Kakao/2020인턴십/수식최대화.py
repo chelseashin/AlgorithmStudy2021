b712354577d7cb8d9df5d47378445e287406a293
chelseashin <chaewonshin95@gmail.com>
2021-05-08 12:49:29 +0900

    210508 | Algorithm Problem Solving

--- chelseashin/PreviousProblems/Kakao/2020인턴십/수식최대화.py
+++ chelseashin/PreviousProblems/Kakao/2020인턴십/수식최대화.py
@@ -1,53 +1,33 @@
+# 거의 1시간 반..? 소요ㅠㅠ
+
 from itertools import permutations
 
-def calc2(op, num1, num2):
-    print("op", op, num1, num2)
-    return 999
-
 def calc(expression, priority):
-    print("priority", priority)
-    stack = []
-    idx = 0
-    while idx < len(priority):
-        op = priority[idx]
-        print("op", op, stack)
-        for ex in expression:
-            print("ex", ex)
-            if not stack:
-                stack.append(ex)
-            elif ex == "+" or ex =="-" or ex == "*":
-                stack.append(ex)
-            else:
-                if stack and stack[-1] == op:
-                    print("calc2 결과", calc2(stack.pop(), stack.pop(), ex))
-        
-        idx += 1
+    for i in range(len(expression)):
+        expression[i] = expression[i].split(priority[1])
+    new_expression = []
+    for i in range(len(expression)):
+        temp = []
+        for e in range(len(expression[i])):
+            if priority[0] in expression[i][e]:
+                temp.append(str(eval(expression[i][e])))
+                continue
+            temp.append(expression[i][e])
+        new_expression.append(temp)
 
-    return 100
-    
+    expression = []
+    for i in range(len(new_expression)):
+        if len(new_expression[i]) > 1:
+            expression.append(str(eval(priority[1].join(new_expression[i]))))
+            continue
+        expression.append(new_expression[i][0])
+    return abs(eval(priority[-1].join(expression)))
 
 def solution(expression):
     answer = 0
-    operators = set()
-    new_expression = []
-    temp = ""
-    for idx in range(len(expression)):
-        if expression[idx] in {"+", "-", "*"} or idx == len(expression)-1:
-            if idx == len(expression)-1:
-                temp += expression[idx]
-                new_expression.append(int(''.join(temp)))
-            else:
-                if temp:
-                    new_expression.append(''.join(temp))
-                operators.add(expression[idx])
-                new_expression.append(expression[idx])
-                temp = []
-        else:
-            temp += expression[idx]
-    print("new_expression", new_expression)
-    for priority in permutations(operators, len(operators)):
+    for priority in permutations("+-*", 3):
+        new_expression = expression.split(priority[-1])
         answer = max(answer, calc(new_expression, priority))
-    print()
     return answer
 
 print(solution("100-200*300-500+20"))
